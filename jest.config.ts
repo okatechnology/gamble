7d1daa19fdfa47944ea12549b76cc15a4d31fab3
okatechnology <okatechnology2020@gmail.com>
2023-11-11 22:27:52 +0900

    テストを書けるように

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+import type { JestConfigWithTsJest } from 'ts-jest';
+
+const jestConfig: JestConfigWithTsJest = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+  },
+};
+
+export default jestConfig;
